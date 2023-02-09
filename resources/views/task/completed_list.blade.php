f27c348a0bbc172eeb4f2a8c17a743ecf25495a5
y.ohta <youseenow9119@gmail.com>
2023-02-09 19:54:47 +0900

    課題提出_4回目

--- resources/views/task/completed_list.blade.php
+++ resources/views/task/completed_list.blade.php
@@ -30,7 +30,7 @@
 <!-- ページネーション -->
 現在 {{ $list->currentPage() }} ページ目<br>
 @if ($list->onFirstPage() === false)
-    <a href="/task/list">最初のページ</a>
+    <a href="/completed_tasks/list">最初のページ</a> 
 @else
     最初のページ
 @endif / 
